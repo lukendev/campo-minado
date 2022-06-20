aaa4413a7300dd36016348065d0408ec6018f6af
lukendev <lucas.azevedo2104@gmail.com>
2022-06-20 16:38:00 -0300

    Classe campo: abrir #02

--- src/br/com/cod3r/cm/modelo/Campo.java
+++ src/br/com/cod3r/cm/modelo/Campo.java
@@ -9,63 +9,79 @@ public class Campo {
 
 	private final int linha;
 	private final int coluna;
-	
+
 	private boolean aberto = false;
 	private boolean minado = false;
 	private boolean marcado = false;
-	
+
 	private List<Campo> vizinhos = new ArrayList<>();
-	
+
 	Campo(int linha, int coluna) {
 		this.linha = linha;
 		this.coluna = coluna;
 	}
-	
+
 	boolean adicionarVizinho(Campo vizinho) {
 		boolean linhaDiferente = linha != vizinho.linha;
 		boolean colunaDiferente = coluna != vizinho.coluna;
 		boolean diagonal = linhaDiferente && colunaDiferente;
-		
+
 		int deltaLinha = Math.abs(linha - vizinho.linha);
 		int deltaColuna = Math.abs(coluna - vizinho.coluna);
-		int deltaGeral = deltaColuna + deltaLinha; 
-		
-		if(deltaGeral == 1 && !diagonal) {
+		int deltaGeral = deltaColuna + deltaLinha;
+
+		if (deltaGeral == 1 && !diagonal) {
 			vizinhos.add(vizinho);
 			return true;
-		} else if(deltaGeral == 2 && diagonal) {
+		} else if (deltaGeral == 2 && diagonal) {
 			vizinhos.add(vizinho);
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 	void alternarMarcacao() {
-		if(!aberto) {
+		if (!aberto) {
 			marcado = !marcado;
 		}
 	}
-	
+
 	boolean abrir() {
-		if(!aberto && !marcado) {
+		if (!aberto && !marcado) {
 			aberto = true;
-			
-			if(minado) {
+
+			if (minado) {
 				throw new ExplosaoException();
 			}
-			
-			if(vizinhancaSegura()) {
+
+			if (vizinhancaSegura()) {
 				vizinhos.forEach(v -> v.abrir());
 			}
-			
+
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 	boolean vizinhancaSegura() {
 		return vizinhos.stream().noneMatch(v -> v.minado);
+	}
+
+	void minar() {
+		minado = true;
+	}
+
+	public boolean isMarcado() {
+		return marcado;
+	}
+	
+	public boolean isAberto() {
+		return aberto;
+	}
+	
+	public boolean isFechado() {
+		return !aberto;
 	}
 }
--- test/br/com/cod3r/cm/modelo/CampoTeste.java
+++ test/br/com/cod3r/cm/modelo/CampoTeste.java
@@ -1,10 +1,13 @@
 package br.com.cod3r.cm.modelo;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import br.com.cod3r.cm.excecao.ExplosaoException;
 
 public class CampoTeste {
 	
@@ -61,5 +64,79 @@ public class CampoTeste {
 		boolean resultado = campo.adicionarVizinho(vizinho);
 		
 		assertFalse(resultado);
+	}
+	
+	@Test
+	void valorPadraoAtributoMarcado() {
+		assertFalse(campo.isMarcado());
+	}
+	
+	@Test
+	void testeAlternarMarcacao() {
+		campo.alternarMarcacao();
+		assertTrue(campo.isMarcado());
+	}
+	
+	@Test
+	void testeAlternarMarcacaoDuasChamadas() {
+		campo.alternarMarcacao();
+		campo.alternarMarcacao();
+		assertFalse(campo.isMarcado());
+	}
+	
+	@Test
+	void testeAbrirNaoMinadoNaoMarcado() {
+		assertTrue(campo.abrir());
+	}
+	
+	@Test
+	void testeAbrirNaoMinadoMarcado() {
+		campo.alternarMarcacao();
+		assertFalse(campo.abrir());
+	}
+	
+	@Test
+	void testeAbrirMinadoMarcado() {
+		campo.alternarMarcacao();
+		campo.minar();
+		assertFalse(campo.abrir());
+	}
+	
+	@Test
+	void testeAbrirMinadoNaoMarcado() {
+		campo.minar();
+		
+		assertThrows(ExplosaoException.class, () -> {
+			assertFalse(campo.abrir());
+		});
+	}
+	
+	@Test
+	void testeAbrirComVizinhos() {
+		Campo campo11 = new Campo(1,1);
+		
+		Campo campo22 = new Campo(2,2);
+		campo.adicionarVizinho(campo11);
+		
+		campo.adicionarVizinho(campo22);
+		campo.abrir();
+		
+		assertTrue(campo22.isAberto() && campo11.isAberto());
+	}
+	
+	@Test
+	void testeAbrirComVizinhos22() {
+		Campo campo11 = new Campo(1,1);
+		Campo campo12 = new Campo(1,1);
+		campo12.minar();
+		
+		Campo campo22 = new Campo(2,2);
+		campo.adicionarVizinho(campo11);
+		campo.adicionarVizinho(campo12);
+		
+		campo.adicionarVizinho(campo22);
+		campo.abrir();
+		
+		assertTrue(campo22.isAberto() && !campo11.isFechado());
 	}
 }
